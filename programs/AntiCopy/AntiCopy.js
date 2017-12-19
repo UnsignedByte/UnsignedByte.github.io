8b3eadf82b9b8e3fa026723e625774c2e3a96e78
Maydoh1239 <34429084+UnsignedByte@users.noreply.github.com>
2017-12-18 18:24:11 -0800

    fix

--- programs/AntiCopy/AntiCopy.js
+++ programs/AntiCopy/AntiCopy.js
@@ -12,10 +12,10 @@ document.body.onmousedown = function (e) {
         isRightMB = e.button == 2;
     return !isRightMB;
 };
-document.onkeypress = function(evt) {
+document.keydown = function(event) {
   alert("keypress")
   var pressed = event.which || event.keyCode;
-  if ( pressed == (91 || 17)){
+  if ( event.ctrlKey || event.metaKey ){
     alert("no command!")
     return false;
   }else{
