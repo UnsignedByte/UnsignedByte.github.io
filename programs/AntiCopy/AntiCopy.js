73f0a3341b8dca1d16305022d1dec9e1bd1d5f2a
Maydoh1239 <34429084+UnsignedByte@users.noreply.github.com>
2017-12-18 18:11:49 -0800

    .

--- programs/AntiCopy/AntiCopy.js
+++ programs/AntiCopy/AntiCopy.js
@@ -12,7 +12,8 @@ document.body.onmousedown = function (e) {
         isRightMB = e.button == 2;
     return !isRightMB;
 }
-window.onkeypress = function(evt) {
+document.onkeypress = function(evt) {
+  alert("keypress")
   var pressed = event.which || event.keyCode;
   if ( pressed == (91 || 17)){
     alert("no command!")
