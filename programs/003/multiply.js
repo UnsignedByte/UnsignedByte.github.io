185141f7ae42d75a77e61def8732a14777439209
Maydoh1239 <34429084+UnsignedByte@users.noreply.github.com>
2018-02-02 13:47:09 -0800

    Update matrix-multiply

--- programs/003/multiply.js
+++ programs/003/multiply.js
@@ -18,7 +18,12 @@ function update(){
 function MatrixUpdate (id){
   var cellId = id.split("-");
   if ( cellId [ 0 ] == 3 ){
-    var tbl = document.getElementById( 'table' + cellId [ 0 ] );
+    var tbl = document.getElementById( 'table3' );
+    for ( var i = 0; i < x2; i++ ){
+      for ( var j = 0; j < y2; j++ ){
+        document.getElementById('2-'+j+'-'+i).setAttribute('value', sumColumn(i));
+      }
+    }
   }else{
     console.log(cellId);
     fixTable();
@@ -29,7 +34,7 @@ function fixTable(){
   console.log(x1,y2);
   for ( var i = 0; i < x1; i++ ){
     for ( var j = 0; j < y2; j++ ){
-      document.getElementById('3-'+j+'-'+i).setAttribute('value', sumRow(j)*sumColumn(i));
+      document.getElementById('3-'+j+'-'+i).innerHTML=sumRow(j)*sumColumn(i);
       console.log(sumRow(j), sumColumn(i));
     }
   }
@@ -56,14 +61,23 @@ function genTable(tableId, x, y){
   for ( var i = 0; i < y; i++ ) {
     var row = tbl.insertRow(tbl.rows.length);
     for ( var j = 0; j < x; j++ ){
-      var input = document.createElement('input');
-      input.setAttribute("class", "square");
-      input.setAttribute("type", "text");
-      input.setAttribute("value", "1");
-      var id = tableId[tableId.length-1]+"-"+i+"-"+j;
-      input.setAttribute("id", id);
-      input.setAttribute("onkeyup", "MatrixUpdate(\""+id+"\")");
-      row.insertCell(j).appendChild(input);
+      if ( tableId[tableId.length-1] == 3 ) {
+        var input = document.createElement('p');
+        input.setAttribute("class", "square");
+        input.innerHTML = '1';
+        var id = tableId[tableId.length-1]+"-"+i+"-"+j;
+        input.setAttribute("id", id);
+        input.setAttribute("onkeyup", "MatrixUpdate(\""+id+"\")");
+        row.insertCell(j).appendChild(input);
+      }else{
+        var input = document.createElement('input');
+        input.setAttribute("class", "square");
+        input.setAttribute("value", 1);
+        var id = tableId[tableId.length-1]+"-"+i+"-"+j;
+        input.setAttribute("id", id);
+        input.setAttribute("onkeyup", "MatrixUpdate(\""+id+"\")");
+        row.insertCell(j).appendChild(input);
+      }
     }
   }
 }
